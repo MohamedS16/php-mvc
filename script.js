083218d32dded50649202495fb4afec57a823d30
MohamedS16 <115186109+MohamedS16@users.noreply.github.com>
2023-08-27 17:07:04 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+// let bb = {
+//     a: 11,
+//     b: 22,
+//     c: 33
+// }
+
+// let x = 1
+// while (x < 10) {
+//     console.log(x)
+//     x++
+// }
+
+// let uu = 'Mohamed'
+
+// for(let x of uu){
+//     console.log(x)
+// }
+
+// let d = 0
+
+// do{
+//     console.log(d + '\nn')
+//     d++
+// }while(d <10)
+
+// var dem = document.querySelector('.dem')
+// var pp = document.querySelectorAll('.pp')
+
+// pp[0].addEventListener('click', ()=>{
+//     window.history.forward()
+// })
+
+// var ch = document.createElement('p')
+// ch.innerHTML = 'OK'
+
+// dem.append(ch)
+
+// var inp = document.querySelector('.inp')
+// inp.onchange = ()=>{
+//     console.log('ok')
+// }
+
+// console.log(window.screenX)
+
+// dem.replaceChild(inp,pp[0])
+
+
+
+var parentt = document.querySelector('.ok')
+var linkk = document.querySelector('.chan')
+var p = document.querySelector('.p')
+
+// p.addEventListener('click',function(){
+//     window.scrollBy(0,500)
+// })
+ 
+
+// window.onscroll = function(){
+//     console.log(window.scrollY)
+// }
+
+
+// p.addEventListener('mouseenter',function(){
+//     console.log('mouse entered')
+// })
+
+
+// p.onclick = function(){
+//     console.log('ok')
+// }
+
+// p.onmouseenter = function(){
+//     console.log('ok')
+// }
+
+
+
+window.sessionStorage.setItem('user','logged')
+// window.localStorage.user = 'logged'
+
+window.localStorage.removeItem('user')
+window.localStorage.clear()
+
+
+console.log(window.localStorage.user)
+
+
+
+
+
